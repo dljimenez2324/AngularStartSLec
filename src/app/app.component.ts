c36e18cb6ee9da0acd62c9d2f5885a8a5471d8d2
dljimenez2324 <dljimenez2324@gmail.com>
2025-03-05 20:52:30 -0800

    Add Bootstrap and Font Awesome, implement ProductListComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <div>My First Component</div>
+      <pm-product-list></pm-product-list>
+    </div>
+    `
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Angular: Getting Started';
 }
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'pm-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Angular: Getting Started';
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product/product-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    
   ],
   imports: [
     BrowserModule
